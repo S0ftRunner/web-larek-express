ebff14065c90f0f5b3054b21e1c2f892f6094a2c
S0ftRunner <benjamin.sokolov19@gmail.com>
2025-08-11 01:51:02 +0300

    fix: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,25 @@
 import express from "express";
 import cors from "cors";
 import productRoute from "./routes/products";
-import userRoute from './routes/user';
+import userRoute from "./routes/user";
 import { configService } from "./config";
 import path from "path";
 
 const app = express();
 
 configService();
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use("/product", productRoute);
 app.use("/auth", userRoute);
