7e6e6277b6577ca4386211142e53181629c897f7
Benjamin <benjamin.sokolov19@gmail.com>
2025-09-04 23:31:55 +0300

    add: чет правил

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 
-export const makeOrder = (req: Request, res: Response) => {
-  return res.send({ message: "Заказ выполнен успешно!" });
+export const makeOrder = async (req: Request, res: Response) => {
+  // Не очень понимаю, зачем в задании сказано про fakerJs, зачем тащить лишние пакеты в проект, когда можно базовое свое написать?
+  // к тому же, проект настроен на commonJs, а последняя версия с ним ругается
+  const randomId =
+    Math.random().toString(36).substring(2) + Date.now().toString(36);
+  return res.send({ total: req.body.total, id: randomId });
 };
